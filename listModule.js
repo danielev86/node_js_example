9275af691408ca8ef97fb55f16b61783e9c0249a
dverr86 <danieleverr86@gmail.com>
2019-01-13 21:12:06 +0100

    Add an element to array

--- listModule.js
+++ listModule.js
@@ -23,7 +23,12 @@ var orderListByLastName = function(data){
 	data.sort( (ele1, ele2) => ele1.last_name.localeCompare(ele2.last_name));
 }
 
+var addElementToArray = function(data, element){
+	data.push(element);
+}
+
 module.exports.printDetails = printDetails;
 module.exports.listSize = listSize;
 module.exports.searchByFirstNameAndLastName = searchByFirstNameAndLastName;
 module.exports.orderListByLastName = orderListByLastName;
+module.exports.addElementToArray = addElementToArray;
--- main.js
+++ main.js
@@ -5,5 +5,6 @@ var list = readWriteModule.parsingJsonFIle('./data/person.json');
 
 var person = listModule.searchByFirstNameAndLastName(list, 'Homer', 'Simpson');
 console.log("Person:" + person.first_name + " " + person.last_name);
+listModule.addElementToArray(list, { "first_name" : "Montgomery", "last_name":"Burns" });
 listModule.orderListByLastName(list);
 listModule.printDetails(list);
