f8ec223d29909d27a843c8853636b2154bfc3fcf
dverr86 <danieleverr86@gmail.com>
2019-01-15 22:24:51 +0100

    Refactoring javascript directory

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-const listModule = require('./listModule');
-const readWriteModule = require('./readWriteModule');
+const listModule = require('./js/listModule');
+const readWriteModule = require('./js/readWriteModule');
+const csvConverter = require('./js/csvConverter');
 
 var list = readWriteModule.parsingJsonFIle('./data/person.json');
 
+//Test list module
 var person = listModule.searchByFirstNameAndLastName(list, 'Homer', 'Simpson');
 console.log("Person:" + person.first_name + " " + person.last_name);
 listModule.addElementToArray(list, { "first_name" : "Montgomery", "last_name":"Burns" });
 listModule.orderListByLastName(list);
 listModule.printDetails(list);
+
+//Test Json Converter
+csvConverter.jsonToCsv(list, ';', "/home/daniele/prova.csv");
+
